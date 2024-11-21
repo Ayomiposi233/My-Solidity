c9325a6b5d11a989e8f1e16096a80686863b32bc
Ayomiposi233 <ayomiposi233@gmail.com>
2024-11-21 16:24:54 +0100

    Arrays & struct assignment

---
+++ quickAsssignment.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract store {
+
+    struct Animal {string name;}
+
+    Animal[] public names_of_animals;
+
+    function add_animal(string memory name) public {
+        names_of_animals.push(Animal(name));
+    }
+}
