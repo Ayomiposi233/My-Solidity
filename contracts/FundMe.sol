f4499173d8b3c51feff2e0b1e29ba7fc08b1d09a
Ayomiposi233 <ayomiposi233@gmail.com>
2024-12-04 22:31:53 +0100

    Fund Me ETH on ZkSync Sepolia complete

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,19 +5,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
+
+error NotOwner();
 
 contract FundMe{
 
     using PriceConverter for uint256;
 
-    uint256 public  minimumUsd =5 * 1e18;
+    uint256 public constant MINIMUM_USD =5 * 1e18;
 
     address[] public funders;
 
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
-    address public Owner;
+    address public immutable Owner;
 
     constructor() {
         Owner = msg.sender;
@@ -25,7 +28,7 @@ contract FundMe{
 
     function fund() public payable {
         msg.value.getConversionRate();
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
@@ -42,9 +45,23 @@ contract FundMe{
         require(CallSuccess, "Call Failed");
     }
 
+    function getVersion() internal view returns(uint256) {
+        return AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF).version();
+    }
+    
+
     modifier onlyOwner() {
-        require(msg.sender == Owner, "Sender Must Be Owner");
+        //require(msg.sender == Owner, "Sender Must Be Owner");
+        if(msg.sender != Owner) {revert NotOwner();}
         _;
+    }
+
+    receive() external payable { 
+        fund();
+    }
+
+    fallback() external payable { 
+        fund();
     }
 
 }
